3705c39be24617fa1d5574dbe899a94f6bc105d6
AAyus-ops <likeeeeeyus@qq.com>
2026-08-12 15:00:15 +0800

    ?? Service Worker(????)

---
+++ sw.js
@@ -0,0 +1,18 @@
+﻿self.addEventListener('push', function (event) {
+  var payload = event.data ? event.data.text() : '该打卡啦！';
+  event.waitUntil(self.registration.showNotification('打卡监督', {
+    body: payload,
+    tag: 'checkin-reminder',
+    renotify: true
+  }));
+});
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+      if ('focus' in list[i]) { list[i].focus(); return undefined; }
+    }
+    return clients.openWindow(self.registration.scope);
+  }));
+});
